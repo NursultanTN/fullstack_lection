074410e1b3074123e650bdc808aef62ee09d7583
bl1tz <Elbar.israilov@gmail.com>
2022-07-11 14:11:27 +0600

    fullstack

---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,81 @@
+import {
+  Alert,
+  Box,
+  Button,
+  Container,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { authContext } from "../../contexts/authContext";
+import Loader from "../Loader/loader";
+
+const Register = () => {
+  const { handleRegister, error, loading } = useContext(authContext);
+  console.log(loading);
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  function handleSave() {
+    if (!email.trim() || !password.trim() || !passwordConfirm.trim()) {
+      //! 1й - способ, собираем данные
+      // const newUser = {
+      //   email,
+      //   password,
+      //   password_confirm: passwordConfirm,
+      // };
+      //! 2й - способ, собираем данные
+      alert("Заполните поля!");
+    } else {
+      let formData = new FormData();
+      formData.append("email", email);
+      formData.append("password", password);
+      formData.append("password_confirm", passwordConfirm);
+      handleRegister(formData, navigate);
+    }
+  }
+  if (loading) {
+    return <Loader />;
+  }
+  return (
+    <Container maxWidth="sm">
+      <Box display={"flex"} flexDirection={"column"} marginTop={"50px"}>
+        <Typography variant="h5">Register</Typography>
+        {error ? (
+          <Box>
+            {error.map((item, index) => (
+              <Alert severity="error" key={item + index}>
+                {item}
+              </Alert>
+            ))}
+          </Box>
+        ) : null}
+        <TextField
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          variant="outlined"
+          label="Email"
+        />
+        <TextField
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          variant="outlined"
+          label="Password"
+        />
+        <TextField
+          value={passwordConfirm}
+          onChange={e => setPasswordConfirm(e.target.value)}
+          variant="outlined"
+          label="Password confirmation"
+        />
+        <Button onClick={handleSave} variant="outlined">
+          Register
+        </Button>
+      </Box>
+    </Container>
+  );
+};
+
+export default Register;
